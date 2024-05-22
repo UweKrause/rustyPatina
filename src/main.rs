71f6908368760e2c999f4895a692cc0db545da05
Uwe Krause <uwe@uwekrau.se>
2024-05-22 22:31:31 +0200

    MVP/POC

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use image::io::Reader as ImageReader;
+
+
 fn main() {
-    println!("Hello, world!");
+    let img = ImageReader::open("landscape.jpg").expect("PROBLEM").decode();
+    let blurred = img.expect("PROBLEM 2").blur(5.5);
+    blurred.save("landscape_blurred.png").expect("PROBLEM 3");
 }
